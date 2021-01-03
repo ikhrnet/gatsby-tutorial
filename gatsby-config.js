675729f530c32d61e4609e55997f58529b5b628e
ikhrnet <ikhrnet@users.noreply.github.com>
2021-01-04 01:37:53 +0900

    Finish tutorial part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,12 +6,16 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: `Pandas Eating Lots`,
+  },
   plugins: [
+    `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModul: `src/utils/typography`,
       },
-    }
+    },
   ],
 }
