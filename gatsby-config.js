275b64ffe154e1d8749f1fddf025736e25ed30fd
ikhrnet <ikhrnet@users.noreply.github.com>
2021-01-05 07:19:51 +0900

    Finish tutorial part 8

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,8 +8,23 @@ module.exports = {
   /* Your site config here */
   siteMetadata: {
     title: `Pandas Eating Lots`,
+    description: `A simple description about pandas eating lots...`,
+    author: `gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
